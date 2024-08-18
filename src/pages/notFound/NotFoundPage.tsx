530949f2a4f8ba5ec3c25ae11148baf36847da75
Siarhei <siarhei.by@gmail.com>
2024-08-18 23:10:07 +0300

    feat: implement not found page

--- src/pages/notFound/NotFoundPage.tsx
+++ src/pages/notFound/NotFoundPage.tsx
@@ -1,3 +1,21 @@
+import { useNavigate } from 'react-router-dom';
+import Button from '@mui/material//Button';
+
+import { PATHS } from '@/router/paths';
+import { Layout } from '@/components';
+import './NotFoundPage.css';
+
 export default function NotFoundPage() {
-  return <h1>Страница не найдена</h1>;
+  const navigate = useNavigate();
+
+  return (
+    <Layout navMenu={false} header={false} footer={false}>
+      <div className="not-found-page">
+        <h2 className="not-found-page__title">Страница не найдена</h2>
+        <Button variant="contained" onClick={() => navigate(PATHS.MAIN)}>
+          На главную
+        </Button>
+      </div>
+    </Layout>
+  );
 }
