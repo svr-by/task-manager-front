f6a0d3d227187e2100d0de403e005d74ba436925
Siarhei <siarhei.by@gmail.com>
2024-08-18 23:18:08 +0300

    feat: implement error page

--- src/pages/error/ErrorPage.tsx
+++ src/pages/error/ErrorPage.tsx
@@ -1,3 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+import Button from '@mui/material//Button';
+
+import { PATHS } from '@/router/paths';
+import { Layout } from '@/components';
+import './ErrorPage.css';
+
 export default function ErrorPage() {
-  return <h1>Ошибка</h1>;
+  const navigate = useNavigate();
+
+  return (
+    <Layout navMenu={false} header={false} footer={false}>
+      <div className="error-page">
+        <h2 className="error-page__title">Что-то пошло не так!</h2>
+        <p className="error-page__message">
+          Попробуйте обновить страницу или свяжитесь с администратором.
+        </p>
+        <Button variant="contained" onClick={() => navigate(PATHS.MAIN)}>
+          На главную
+        </Button>
+      </div>
+    </Layout>
+  );
 }
